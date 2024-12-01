6acd381550de1a2b337118d2c1a876572b18267b
Jonathan Lin <jlin@winteroftext.ca>
2024-11-30 16:20:15 -0800

    spacing nits

--- day0/src/day0.adb
+++ day0/src/day0.adb
@@ -1,10 +1,9 @@
--- My first program in Ada
+--  My first program in Ada
 
 with Ada.Text_IO; use Ada.Text_IO;
 
-
 procedure Day0 is
 begin
-   Put_Line("Hello, World!");
-   -- null;
+   Put_Line ("Hello, World!");
+   --  null;
 end Day0;
