8b6a313d4099eba9edd223bbe4eaf65745d2f675
Jonathan Lin <jlin@winteroftext.ca>
2024-11-30 16:30:15 -0800

    simple variables, if branching

--- day0/src/day0.adb
+++ day0/src/day0.adb
@@ -3,7 +3,15 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Day0 is
+-- Variable declarations
+   A,B : Integer := 0;
+   C   : Integer := 1000;
+   D   : Boolean := false;
 begin
-   Put_Line ("Hello, World!");
-   --  null;
+   Put_Line("Hello, World!");
+
+   if C > A then
+     Put_Line("C is greater than A");
+   end if;
+
 end Day0;
